5a76a47e8515f3368d8be6d8d6a040fa6de06298
EduConnectRuben <educonnetruben@gmail.com>
2026-03-28 10:46:40 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 const API = "https://analisis-y-diseno-de-sistemas-ii.onrender.com";
 
-// REGISTRO
 async function registrar() {
+    console.log("Registrando...");
+
     const email = document.getElementById("reg_email").value;
     const password = document.getElementById("reg_password").value;
 
-    const res = await fetch(`${API}/registro`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-    });
+    try {
+        const res = await fetch(`${API}/registro`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
 
-    const data = await res.json();
-    alert(JSON.stringify(data));
+        const data = await res.json();
+        alert(JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+        alert("Error al registrar");
+    }
 }
 
-// LOGIN
 async function login() {
+    console.log("Login...");
+
     const email = document.getElementById("login_email").value;
     const password = document.getElementById("login_password").value;
 
-    const res = await fetch(`${API}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-    });
+    try {
+        const res = await fetch(`${API}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
 
-    const data = await res.json();
-    alert(JSON.stringify(data));
+        const data = await res.json();
+        alert(JSON.stringify(data));
+    } catch (error) {
+        console.error(error);
+        alert("Error en login");
+    }
 }
