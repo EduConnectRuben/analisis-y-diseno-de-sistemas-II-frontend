895ea48c66ea876030cd02d760bd7464c4b28fd1
EduConnectRuben <educonnetruben@gmail.com>
2026-03-28 11:14:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,62 @@
 const API = "https://analisis-y-diseno-de-sistemas-ii.onrender.com";
 
 async function registrar() {
-    console.log("Registrando...");
-
     const email = document.getElementById("reg_email").value;
     const password = document.getElementById("reg_password").value;
 
     try {
-        const res = await fetch(`${API}/registro`, {
+        const res = await fetch(API + "/registro", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ email, password })
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
         });
 
         const data = await res.json();
-        alert(JSON.stringify(data));
+
+        if (res.ok) {
+            alert("Usuario registrado correctamente");
+        } else {
+            alert("Error: " + data.detail);
+        }
+
     } catch (error) {
-        console.error(error);
-        alert("Error al registrar");
+        document.getElementById("msg").innerText = "Error conexión backend";
     }
 }
 
 async function login() {
-    console.log("Login...");
-
     const email = document.getElementById("login_email").value;
     const password = document.getElementById("login_password").value;
 
     try {
-        const res = await fetch(`${API}/login`, {
+        const res = await fetch(API + "/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ email, password })
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
         });
+
+        const data = await res.json();
+
+        if (res.ok) {
+            alert("Login correcto");
+        } else {
+            alert("Error: " + data.detail);
+        }
+
+    } catch (error) {
+        document.getElementById("msg").innerText = "Error conexión backend";
+    }
+}
 
         const data = await res.json();
         alert(JSON.stringify(data));
